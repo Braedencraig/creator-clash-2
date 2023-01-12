36c61f72b78764e42368ed71108f2315a52e7113
Braeden Craig <braeden.craig@gmail.com>
2023-01-11 23:14:21 -0500

    addition announcer component

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import "../styles/globals.css";
+import { Poppins } from "@next/font/google";
 import { GoogleAnalytics } from "nextjs-google-analytics";
+
+const poppins = Poppins({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }) {
   return (
-    <div>
+    <div className={poppins.className}>
       <GoogleAnalytics trackPageViews />
       <Component {...pageProps} />
     </div>
