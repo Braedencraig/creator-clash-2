337381efba7d518591000e7c393159184d47a0bb
Braeden Craig <braeden.craig@gmail.com>
2023-01-17 21:29:35 -0500

    update faq

--- components/faq.js
+++ components/faq.js
@@ -5,166 +5,131 @@ export default function Faq() {
   return (
     <div className="faq-bg">
       <div className="faq">
-        <h2>Frequently Asked Questions</h2>
-        <Accordion isFaq title="How will I get my VIP information?">
+        <h2>Tickets</h2>
+        <Accordion isFaq title="When does Creator Clash 2 go on sale?">
           <p>
-            VIP tickets are non-transferable at this time, unless the person you
-            purchase your ticket from emails vip@realgoodtouring.com​ to
-            transfer their bundle to you.
+            Event tickets and pay-per-view tickets will be available for
+            purchase on January 27, 2023 at 10:00am ET/7:00am PT.
+          </p>
+        </Accordion>
+        {/* <Accordion isFaq title="Where do I purchase event tickets to CC2?">
+          <p>
+            Event tickets to attend Creator Clash 2 in Tampa, Florida may be
+            purchased here [[TICKETMASTER LINK HYPERLINKED]].
+          </p>
+        </Accordion> */}
+        <Accordion
+          isFaq
+          title="I’m having trouble purchasing event tickets on Ticketmaster - what do I do?"
+        >
+          <p>
+            Please reference Ticketmaster’s{" "}
+            <a
+              href="https://help.ticketmaster.com/s/?language=en_US"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              FAQ
+            </a>{" "}
+            for assistance with purchasing event tickets. If you require further
+            assistance, please{" "}
+            <a
+              href="https://ticketmaster-us.zendesk.com/hc/en-us/requests/new?ticket_form_id=5957004019217"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              contact Ticketmaster fan support
+            </a>{" "}
+            directly.
+          </p>
+        </Accordion>
+        <Accordion isFaq title="What does my event ticket purchase include?">
+          <p>
+            Event tickets include admission to the Creator Clash 2 live event on
+            April 15, 2023 at Amalie Arena in Tampa, Florida.
           </p>
           <p>
-            If you are a parent who purchased a ticket for your child, or you
-            bought the tickets as a gift and they are listed in your name and
-            not the recipient&apos;s, please email{" "}
-            <a href="mailto:vip@realgoodtouring.com">vip@realgoodtouring.com</a>{" "}
-            ​to explore your options.
+            Can’t make it to Tampa? Purchase pay-per-view tickets for access to
+            the livestream hosted by Moment,{" "}
+            <a
+              href="https://www.moment.co/creatorclash"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              here
+            </a>
+            .
           </p>
+        </Accordion>
+        <Accordion isFaq title="Where do I purchase PPV tickets?">
+          <p>
+            Pay-per-view tickets may be purchased{" "}
+            <a
+              href="https://www.moment.co/creatorclash"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              here
+            </a>
+            .
+          </p>
+          <p>
+            Please note - pay-per-view tickets do not include admission to the
+            in-person event at Amalie Arena. Event tickets sold separately.
+            {/* [[TICKETMASTER LINK HYPERLINKED]] */}
+          </p>
+        </Accordion>
+        <Accordion isFaq title="What does my PPV ticket purchase include?">
+          <p>
+            PPV tickets include access to view the official Creator Clash 2
+            livestream hosted by Moment.
+          </p>
+          {/* <p>
+            Looking for tickets to attend Creator Clash 2 in Tampa? Purchase
+            event tickets for admission to the in-person event here
+            [[TICKETMASTER TICKET LINK HYPERLINKED]].
+          </p> */}
         </Accordion>
         <Accordion
           isFaq
-          title="I missed my VIP. Can you ship the merchandise bundle to me?"
+          title="I’m having trouble purchasing PPV tickets/accessing the livestream - what do I do?"
         >
           <p>
-            VIP tickets are non-transferable at this time, unless the person you
-            purchase your ticket from emails vip@realgoodtouring.com​ to
-            transfer their bundle to you.
-          </p>
-          <p>
-            If you are a parent who purchased a ticket for your child, or you
-            bought the tickets as a gift and they are listed in your name and
-            not the recipient&apos;s, please email{" "}
-            <a href="mailto:vip@realgoodtouring.com">vip@realgoodtouring.com</a>{" "}
-            ​to explore your options.
-          </p>
-        </Accordion>
-        <Accordion
-          isFaq
-          title="Can I bring gifts for the creators to the VIP program?"
-        >
-          <p>
-            VIP tickets are non-transferable at this time, unless the person you
-            purchase your ticket from emails vip@realgoodtouring.com​ to
-            transfer their bundle to you.
-          </p>
-          <p>
-            If you are a parent who purchased a ticket for your child, or you
-            bought the tickets as a gift and they are listed in your name and
-            not the recipient&apos;s, please email{" "}
-            <a href="mailto:vip@realgoodtouring.com">vip@realgoodtouring.com</a>{" "}
-            ​to explore your options.
+            Please reference {""}
+            <a
+              href="https://www.moment.co/faq?type=fan"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Moment’s FAQ
+            </a>{" "}
+            for assistance troubleshooting any issues with purchasing a PPV
+            ticket or accessing the livestream.
           </p>
         </Accordion>
-        <Accordion
-          isFaq
-          title="Is there a meet and greet included in my VIP program?"
-        >
+        <h2>Event Info</h2>
+        <Accordion isFaq title="Where will Creator Clash 2 take place?">
           <p>
-            VIP tickets are non-transferable at this time, unless the person you
-            purchase your ticket from emails vip@realgoodtouring.com​ to
-            transfer their bundle to you.
-          </p>
-          <p>
-            If you are a parent who purchased a ticket for your child, or you
-            bought the tickets as a gift and they are listed in your name and
-            not the recipient&apos;s, please email{" "}
-            <a href="mailto:vip@realgoodtouring.com">vip@realgoodtouring.com</a>{" "}
-            ​to explore your options.
+            Creator Clash 2 will take place at Amalie Arena in Tampa, Florida.
           </p>
         </Accordion>
-        <Accordion
-          isFaq
-          title="I can't make it to my VIP program. What are my options?"
-        >
+        <Accordion isFaq title="When is Creator Clash 2?">
           <p>
-            VIP tickets are non-transferable at this time, unless the person you
-            purchase your ticket from emails vip@realgoodtouring.com​ to
-            transfer their bundle to you.
-          </p>
-          <p>
-            If you are a parent who purchased a ticket for your child, or you
-            bought the tickets as a gift and they are listed in your name and
-            not the recipient&apos;s, please email{" "}
-            <a href="mailto:vip@realgoodtouring.com">vip@realgoodtouring.com</a>{" "}
-            ​to explore your options.
+            The Creator Clash 2 event & livestream will take place on Saturday,
+            April 15, 2023.
           </p>
         </Accordion>
-        <Accordion
-          isFaq
-          title="I bought multiple VIP tickets under one email for my full party. Will my friends attending have issues getting in since my name is on the ticket?"
-        >
+        <Accordion isFaq title="Will x-item be allowed in the arena?">
           <p>
-            VIP tickets are non-transferable at this time, unless the person you
-            purchase your ticket from emails vip@realgoodtouring.com​ to
-            transfer their bundle to you.
-          </p>
-          <p>
-            If you are a parent who purchased a ticket for your child, or you
-            bought the tickets as a gift and they are listed in your name and
-            not the recipient&apos;s, please email{" "}
-            <a href="mailto:vip@realgoodtouring.com">vip@realgoodtouring.com</a>{" "}
-            ​to explore your options.
-          </p>
-        </Accordion>
-        <Accordion
-          isFaq
-          title="I have a VIP question that isn't answered here. Who can I ask for help?"
-        >
-          <p>
-            VIP tickets are non-transferable at this time, unless the person you
-            purchase your ticket from emails vip@realgoodtouring.com​ to
-            transfer their bundle to you.
-          </p>
-          <p>
-            If you are a parent who purchased a ticket for your child, or you
-            bought the tickets as a gift and they are listed in your name and
-            not the recipient&apos;s, please email{" "}
-            <a href="mailto:vip@realgoodtouring.com">vip@realgoodtouring.com</a>{" "}
-            ​to explore your options.
-          </p>
-        </Accordion>
-        <Accordion isFaq title="How do I get a refund for my VIP ticket?">
-          <p>
-            VIP tickets are non-transferable at this time, unless the person you
-            purchase your ticket from emails vip@realgoodtouring.com​ to
-            transfer their bundle to you.
-          </p>
-          <p>
-            If you are a parent who purchased a ticket for your child, or you
-            bought the tickets as a gift and they are listed in your name and
-            not the recipient&apos;s, please email{" "}
-            <a href="mailto:vip@realgoodtouring.com">vip@realgoodtouring.com</a>{" "}
-            ​to explore your options.
-          </p>
-        </Accordion>
-        <Accordion
-          isFaq
-          title="I bought a GA ticket but want to upgrade to a VIP ticket. How do I do that?"
-        >
-          <p>
-            VIP tickets are non-transferable at this time, unless the person you
-            purchase your ticket from emails vip@realgoodtouring.com​ to
-            transfer their bundle to you.
-          </p>
-          <p>
-            If you are a parent who purchased a ticket for your child, or you
-            bought the tickets as a gift and they are listed in your name and
-            not the recipient&apos;s, please email{" "}
-            <a href="mailto:vip@realgoodtouring.com">vip@realgoodtouring.com</a>{" "}
-            ​to explore your options.
-          </p>
-        </Accordion>
-        <Accordion isFaq title="How will I get my VIP information?">
-          <p>
-            VIP tickets are non-transferable at this time, unless the person you
-            purchase your ticket from emails vip@realgoodtouring.com​ to
-            transfer their bundle to you.
-          </p>
-          <p>
-            If you are a parent who purchased a ticket for your child, or you
-            bought the tickets as a gift and they are listed in your name and
-            not the recipient&apos;s, please email{" "}
-            <a href="mailto:vip@realgoodtouring.com">vip@realgoodtouring.com</a>{" "}
-            ​to explore your options.
+            Please reference Amalie Arena’s{" "}
+            <a
+              href="https://www.amaliearena.com/arena-info"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              arena info guide
+            </a>{" "}
+            for further information regarding the venue’s policies & procedures.
           </p>
         </Accordion>
       </div>
